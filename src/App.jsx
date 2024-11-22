d0b847ca4ceedce2304a61de9c408f100cf5a30e
Massa-Albani4 <massa.albani0213@gmail.com>
2024-11-22 18:39:10 +0400

    Add design system

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <div className="text-red-500 text-6xl">Im a legend</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: "#686868",
+      },
+    },
+    fontFamily: {
+      body: ["Open Sans"],
+    },
+  },
+  plugins: [],
+};

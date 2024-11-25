77bf8e66a20aa3bfb9728a2373741e4ea57b5bec
Massa-Albani4 <massa.albani0213@gmail.com>
2024-11-25 18:46:18 +0400

    Add colors to design system

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import ReactPlayer from "react-player/youtube";
+import movieTrailer from "movie-trailer";
+
 function App() {
   return (
-    <div className="h-screen flex font-body justify-center items-center">
+    <div className="h-screen flex font-body justify-center items-center bg-dark-green">
       <div className="flex flex-col">
-        <h1 className="text-3xl">search for any movies/shows</h1>
-        <input className="bg-slate-500" type="search" />
+        <h1 className="text-xl text-light-green">
+          search for any movies/shows
+        </h1>
+        <input className="border-2 border-light-green" type="search" />
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+          controls={true}
+        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        gray: "#686868",
-      },
       screens: {
         xl: "1440px",
         "2xl": "1920px",
@@ -13,6 +10,10 @@ export default {
     },
     fontFamily: {
       body: ["Open Sans"],
+    },
+    colors: {
+      "light-green": "#EDFFAB",
+      "dark-green": "#30362F",
     },
   },
   plugins: [],

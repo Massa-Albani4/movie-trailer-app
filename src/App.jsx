c98cc814a00cdfc6ac2bdc15b4e1137442460389
Massa-Albani4 <massa.albani0213@gmail.com>
2024-11-25 19:00:49 +0400

    Add title typography styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import movieTrailer from "movie-trailer";
 
 function App() {
   return (
-    <div className="h-screen flex font-body justify-center items-center bg-dark-green">
-      <div className="flex flex-col">
+    <div className="h-screen flex font-body justify-center bg-dark-green">
+      <div className="flex flex-col mt-44">
         <h1 className="text-xl text-light-green">
           search for any movies/shows
         </h1>
         <input className="border-2 border-light-green" type="search" />
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-          controls={true}
-        />
       </div>
     </div>
   );

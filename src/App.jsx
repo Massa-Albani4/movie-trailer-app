56d29359f10f487d42c2f41329a9bb9d3e1bcaa4
Massa Albani <massa.albani0213@gmail.com>
2025-11-04 22:58:52 +0400

    Update search input padding for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -16,42 +16,44 @@ function App() {
   }
 
   return (
-    <div className="h-screen flex flex-col font-body md:pt-10 justify-center items-center bg-black">
-      <div className="flex flex-col items-center 2xl:mt-1 w-[87%] 2xl:w-[31%]">
-        <h1 className="text-xl 2xl:text-[2rem] lg:text-2xl mb-[1.38rem] 2xl:mb-8 text-white">
-          search for any movies/shows
-        </h1>
-        <div
-          className="border-[1px] w-[84%] xl:w-[28%] md:w-[36%] xl:px-4 2xl:w-full mb-14 lg:mb-5 md:mb-2 xl:mb-2 2xl:mb-[4.88rem] cursor-pointer flex flex-row rounded-[5px] px-3 2xl:px-5 py-[.3rem] 2xl:py-[.5rem] xl:pt-[.2rem] text-light-green"
-          autoComplete="off"
-        >
-          <input
-            className="bg-transparent pt-[.1rem] w-full flex justify-center items-center focus:outline-none focus:ring-0 placeholder:text-white placeholder:text-sm 2xl:placeholder:text-base md:placeholder:text-sm xl:placeholder:text-base 2xl:mb-[.13rem] text-xs xl:text-base lg:text-sm 2xl:text-xl md:text-sm text-white"
-            type="text"
-            placeholder="Search"
-            onChange={(e) => {
-              setVideo(e.target.value);
-            }}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                handleSearch();
-              }
-            }}
-          />
-          <button
-            className="bg-transparent border-none focus:outline-none"
-            onClick={handleSearch}
+    <div className="h-screen flex flex-col font-body md:pt-10 lg:pt-4 xl:pt-0 justify-center items-center bg-black">
+      <div className="flex flex-col items-center justify-center w-full xl:gap-10">
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="text-xl 2xl:text-[2rem] lg:text-2xl mb-[1.38rem] 2xl:mb-8 text-white">
+            search for any movies/shows
+          </h1>
+          <div
+            className="border-[1px] w-[84%] xl:w-[24%] md:w-[36%] xl:px-3 mb-14 lg:mb-5 md:mb-2 xl:mb-2 cursor-pointer flex flex-row rounded-[5px] px-3 xl: pb-[.3rem] pt-[.1rem] text-light-green"
+            autoComplete="off"
           >
-            <img
-              className="size-5 2xl:size-5 2xl:h-10"
-              src={searchIcon}
-              alt="Search icon"
+            <input
+              className="bg-transparent pt-[.1rem] w-full xl:w-full flex justify-center items-center focus:outline-none focus:ring-0 placeholder:text-white placeholder:text-sm 2xl:placeholder:text-base md:placeholder:text-sm xl:placeholder:text-base 2xl:mb-[.13rem] text-xs xl:text-base lg:text-sm 2xl:text-xl md:text-sm text-white"
+              type="text"
+              placeholder="Search"
+              onChange={(e) => {
+                setVideo(e.target.value);
+              }}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                }
+              }}
             />
-          </button>
+            <button
+              className="bg-transparent border-none focus:outline-none"
+              onClick={handleSearch}
+            >
+              <img
+                className="size-5 2xl:size-5 2xl:h-10 mt-1"
+                src={searchIcon}
+                alt="Search icon"
+              />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="w-[85%] md:w-[60%] md:h-[21rem] lg:h-[16.2rem] xl:w-[50%] xl:h-[22rem] 2xl:w-[20%] h-[19rem] mb-24 lg:mb-2 md:mb-5 2xl:h-[20rem]">
-        <ReactPlayer url={videoURL} width={"100%"} height={"100%"} />
+        <div className="w-[85%] md:w-[60%] md:h-[21rem] lg:h-[16.2rem] xl:w-[50%] xl:h-[22rem] 2xl:w-[20%] h-[19rem] mb-24 lg:mb-2 md:mb-5 2xl:h-[20rem]">
+          <ReactPlayer url={videoURL} width={"100%"} height={"100%"} />
+        </div>
       </div>
     </div>
   );

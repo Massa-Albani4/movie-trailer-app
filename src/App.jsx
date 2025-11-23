085391433d54599a03a2a2dad3c6c0eb92827596
Massa Albani <massa.albani0213@gmail.com>
2025-11-23 13:27:21 +0400

    Update input styles for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,15 @@ function App() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black font-body md:pt-10 lg:gap-6 lg:pt-4 xl:gap-10 xl:pt-0">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="mb-[1.38rem] text-xl text-white sm:mb-4 sm:text-lg lg:mb-4 lg:text-2xl 2xl:mb-8 2xl:text-[2rem]">
+        <h1 className="mb-[1.38rem] text-xl text-white sm:mb-4 lg:mb-4 lg:text-2xl 2xl:mb-8 2xl:text-[2rem]">
           search for any movies/shows
         </h1>
         <div
-          className="text-light-green mb-14 flex w-[84%] cursor-pointer flex-row rounded-[5px] border-[1px] px-4 sm:mb-10 sm:h-8 sm:w-72 sm:px-3 md:mb-2 lg:mb-5 lg:w-80 xl:my-4 xl:mb-2 xl:h-10 xl:w-80 xl:rounded-md xl:px-4 xl:py-1"
+          className="text-light-green mb-14 flex w-full cursor-pointer flex-row rounded-md border-[1px] px-3 sm:mb-10 sm:h-10 md:mb-2 lg:mb-5 lg:w-80 xl:my-4 xl:mb-2 xl:h-10 xl:w-80 xl:rounded-md xl:px-4 xl:py-1"
           autoComplete="off"
         >
           <input
-            className="flex w-full items-center justify-center bg-transparent text-xs text-white placeholder:text-sm placeholder:text-white focus:outline-none focus:ring-0 sm:text-sm sm:placeholder:text-sm md:text-sm md:placeholder:text-sm lg:w-[28rem] lg:text-sm xl:w-full xl:text-lg xl:placeholder:text-lg 2xl:mb-[.13rem] 2xl:text-xl 2xl:placeholder:text-base"
+            className="my-2 flex w-full items-center justify-center bg-transparent text-base text-white placeholder:text-base placeholder:text-white focus:outline-none focus:ring-0 md:text-sm md:placeholder:text-sm lg:w-[28rem] lg:text-sm xl:w-full xl:text-lg xl:placeholder:text-lg 2xl:mb-[.13rem] 2xl:text-xl 2xl:placeholder:text-base"
             type="text"
             placeholder="Search"
             onChange={(e) => {
@@ -43,7 +43,7 @@ function App() {
             onClick={handleSearch}
           >
             <img
-              className="size-5 sm:mb-0 sm:mt-[.2rem] sm:size-4 lg:mt-0 lg:size-7 xl:size-6"
+              className="size-6 lg:mt-0 lg:size-7 xl:size-6"
               src={searchIcon}
               alt="Search icon"
             />

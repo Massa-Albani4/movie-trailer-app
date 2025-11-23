d17a2b129788e4bc0fad37a0c594b6a6550ce674
Massa Albani <massa.albani0213@gmail.com>
2025-11-23 11:34:54 +0400

    Update input styles for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           search for any movies/shows
         </h1>
         <div
-          className="text-light-green mb-14 flex w-[84%] cursor-pointer flex-row rounded-[5px] border-[1px] px-3 pb-[.3rem] pt-[.1rem] sm:mb-10 sm:h-8 sm:w-72 sm:px-3 sm:pb-1 md:mb-2 lg:mb-5 lg:w-80 xl:mb-2 xl:h-[2.1rem] xl:w-full xl:px-3 xl:pb-[.4rem]"
+          className="text-light-green mb-14 flex w-[84%] cursor-pointer flex-row rounded-[5px] border-[1px] px-4 sm:mb-10 sm:h-8 sm:w-72 sm:px-3 md:mb-2 lg:mb-5 lg:w-80 xl:my-4 xl:mb-2 xl:h-10 xl:w-80 xl:rounded-md xl:px-4 xl:py-1"
           autoComplete="off"
         >
           <input
-            className="flex w-full items-center justify-center bg-transparent pt-[.1rem] text-xs text-white placeholder:text-sm placeholder:text-white focus:outline-none focus:ring-0 sm:pt-[.2rem] sm:text-sm sm:placeholder:text-sm md:text-sm md:placeholder:text-sm lg:w-[28rem] lg:text-sm xl:w-full xl:text-base xl:placeholder:text-base 2xl:mb-[.13rem] 2xl:text-xl 2xl:placeholder:text-base"
+            className="flex w-full items-center justify-center bg-transparent text-xs text-white placeholder:text-sm placeholder:text-white focus:outline-none focus:ring-0 sm:text-sm sm:placeholder:text-sm md:text-sm md:placeholder:text-sm lg:w-[28rem] lg:text-sm xl:w-full xl:text-lg xl:placeholder:text-lg 2xl:mb-[.13rem] 2xl:text-xl 2xl:placeholder:text-base"
             type="text"
             placeholder="Search"
             onChange={(e) => {
@@ -43,7 +43,7 @@ function App() {
             onClick={handleSearch}
           >
             <img
-              className="size-5 sm:mb-0 sm:mt-[.2rem] sm:size-4 lg:mt-0 lg:size-7 xl:mt-1 xl:size-5"
+              className="size-5 sm:mb-0 sm:mt-[.2rem] sm:size-4 lg:mt-0 lg:size-7 xl:size-6"
               src={searchIcon}
               alt="Search icon"
             />
